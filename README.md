8011bd263932ee2209dc2363a7387d68e76e0c29
Michael C Li <michael.li@medium.one>
2019-04-09 10:58:37 -0700

    Add the CRC table to generate CRC checksum.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aecloud2_arduino_cpp_sht31_i2c1_grove
 AE Cloud2 Arduino firmware with SHT31 example sketch.
+
+Features: Added the code for CRC8 verification with two methods (xor/shift or table method).
--- src/SHT31.cpp
+++ src/SHT31.cpp
@@ -37,12 +37,43 @@ THE SOFTWARE.
 #include "SERIAL1.h"
 extern SERIAL1 Serial;   //UART 1 Serial communication
 
-
-#define  POLYBYTE  0x31  // SHT31 sensor
+#define  CRC_TABLE       // Use the table to speed up CRC generation
+#define  POLYBYTE  0x31  // SHT31 sensor 8 bits CRC polynomial  (x8 + x5 + x4 + 1)
 
 #define  CRC_INITIAL 0xFF
 #define  CRC_FINALXOR 0x00
+
+unsigned char crctable[256] =
+{     0,     49,     98,     83,    196,    245,    166,    151,    185,    136,
+    219,    234,    125,     76,     31,     46,     67,    114,     33,     16,
+    135,    182,    229,    212,    250,    203,    152,    169,     62,     15,
+     92,    109,    134,    183,    228,    213,     66,    115,     32,     17,
+     63,     14,     93,    108,    251,    202,    153,    168,    197,    244,
+    167,    150,      1,     48,     99,     82,    124,     77,     30,     47,
+    184,    137,    218,    235,     61,     12,     95,    110,    249,    200,
+    155,    170,    132,    181,    230,    215,     64,    113,     34,     19,
+    126,     79,     28,     45,    186,    139,    216,    233,    199,    246,
+    165,    148,      3,     50,     97,     80,    187,    138,    217,    232,
+    127,     78,     29,     44,      2,     51,     96,     81,    198,    247,
+    164,    149,    248,    201,    154,    171,     60,     13,     94,    111,
+     65,    112,     35,     18,    133,    180,    231,    214,    122,     75,
+     24,     41,    190,    143,    220,    237,    195,    242,    161,    144,
+      7,     54,    101,     84,     57,      8,     91,    106,    253,    204,
+    159,    174,    128,    177,    226,    211,     68,    117,     38,     23,
+    252,    205,    158,    175,     56,      9,     90,    107,     69,    116,
+     39,     22,    129,    176,    227,    210,    191,    142,    221,    236,
+    123,     74,     25,     40,      6,     55,    100,     85,    194,    243,
+    160,    145,     71,    118,     37,     20,    131,    178,    225,    208,
+    254,    207,    156,    173,     58,     11,     88,    105,      4,     53,
+    102,     87,    192,    241,    162,    147,    189,    140,    223,    238,
+    121,     72,     27,     42,    193,    240,    163,    146,      5,     52,
+    103,     86,    120,     73,     26,     43,    188,    141,    222,    239,
+    130,    179,    224,    209,     70,    119,     36,     21,     59,     10,
+     89,    104,    255,    206,    157,    172};
+
+
 unsigned char gen_crc(unsigned char *array, int len);
+unsigned char gen_crc_from_table(unsigned char *array, int len);
 // -------------------------------
 
 // use this declaration for this i2c device that owns the i2c port exclusively
@@ -140,7 +171,12 @@ boolean SHT31::getTempHum(void) {
   }
   Serial.println(" ");
 
+#ifndef CRC_TABLE
   crc = gen_crc(readbuffer,2);
+#else
+  crc = gen_crc_from_table(readbuffer,2);
+#endif
+
   Serial.print("Calculated Temperature CRC:");
   Serial.print(crc,HEX);
 
@@ -156,7 +192,12 @@ boolean SHT31::getTempHum(void) {
   }
   Serial.println(" ");
 
+#ifndef CRC_TABLE
   crc = gen_crc(readbuffer+3,2);
+#else
+  crc = gen_crc_from_table(readbuffer+3,2);
+#endif
+
   Serial.print("Calculated Humidity CRC:");
   Serial.print(crc,HEX);
   if (crc == readbuffer[5])
@@ -209,6 +250,8 @@ void SHT31::writeCommand(uint16_t cmd) {
 //
 unsigned char gen_crc(unsigned char *array, int len) {
 
+    Serial.println("Use XOR and shift operator to generate CRC.");
+
     unsigned char crc = CRC_INITIAL;
 
     for (int index=0; index < len; index++) {
@@ -222,5 +265,21 @@ unsigned char gen_crc(unsigned char *array, int len) {
             //printf("i = %d \tcrc = %2X  \n",i, (crc & 0xFF));
         }
     }
+    return crc ^ CRC_FINALXOR;
+}
+
+unsigned char gen_crc_from_table(unsigned char *array, int len) {
+
+    Serial.println("Use CRC table to gernate CRC.");
+
+    unsigned char crc = CRC_INITIAL;
+    unsigned char data = 0;
+
+    for (int index=0; index < len; index++) {
+        data = crc ^ array[index];
+        crc = crctable[data];
+        //printf("index = %d \tcrc = %2X  \n",index, (crc & 0xFF));
+    }
+
     return crc ^ CRC_FINALXOR;
 }
